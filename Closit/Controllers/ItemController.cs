8fd171d9a08ee747a72d39871e8620ad654724df
Ashassins <ashenassassinmc@gmail.com>
2023-01-21 08:16:52 -0500

    Item Controller Barebones

--- Closit/Controllers/ItemController.cs
+++ Closit/Controllers/ItemController.cs
@@ -1,9 +1,27 @@
-using System;
-using System.Collections;
-using System.IO;
-// using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc; // the one that holds controller
+
 namespace Closit.Controllers {
-    // public class ItemController : Controller {
-    //     // [HttpPost, ActionName("Create")]
-    // }
+    public class ItemController : Controller {
+        [HttpPost, ActionName("Create")]
+        public void CreateItem() {
+
+        }
+
+        [HttpPost, ActionName("Read")]
+        public void ReadItem() {
+
+        }
+        [HttpPost, ActionName("Update")]
+        public void UpdateItem() {
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public void DeleteItem() {
+
+        }
+    }
 }
