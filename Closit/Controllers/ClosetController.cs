d622c3488278fbe19212795ce0cd25b60a1fe434
Ashassins <ashenassassinmc@gmail.com>
2023-01-21 10:15:55 -0500

    add closetcontroller template

--- Closit/Controllers/ClosetController.cs
+++ Closit/Controllers/ClosetController.cs
@@ -5,23 +5,24 @@ using Microsoft.AspNetCore.Mvc; // the one that holds controller
 
 namespace Closit.Controllers {
     public class ClosetController : Controller {
-        [HttpPost, ActionName("Create")]
-        public void CreateItem() {
+        // [HttpGet ("/browse")]
+        // public async Task<IActionResult> ReadItems() {
+        // }
 
-        }
+        // [HttpPost ("/browse/insert")]
+        // public async Task<IActionResult> InsertItem (Cloth cloth) {
+        //     // Run SQL Procedure to save.
+        // }
 
-        [HttpPost, ActionName("Read")]
-        public void ReadItem() {
+        // [HttpPost ("/browse/{id}/update")]
+        // public async Task<IActionResult> UpdateItem(Cloth cloth) {
 
-        }
-        [HttpPost, ActionName("Update")]
-        public void UpdateItem() {
+        // }
 
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public void DeleteItem() {
-
-        }
+        // [HttpPost ("/browse/{id}/delete")]
+        // public async Task<IActionResult> DeleteItem(Cloth cloth) {
+        //     // int deleteId = cloth.getUUID();
+        //     // Run SQL Procedure to delete
+        // }
     }
 }
