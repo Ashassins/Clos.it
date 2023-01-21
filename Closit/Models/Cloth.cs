7a3bb6919813a9511cbf4433295dc5016dc6cc54
Kenyon-Kong <kong98@purdue.edu>
2023-01-21 04:02:42 -0500

    add Data, Cloth, Outfits Objects

---
+++ Closit/Models/Cloth.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Closit.Models
+{
+    public class Cloth {
+        public String UUID;
+        public String listingName {get; set; }
+        public String givenName {get; set; }
+        public allEnums.Color[] colorList {get; set; }
+        public allEnums.Season season {get; set; }
+        public DateTime purchaseDate {get; set; }
+        public allEnums.Gender gender {get; set; }
+        public String brand {get; set; }
+        public String tags {get; set; }
+    }
+
+}
---
+++ Closit/Models/allEnums.cs
@@ -0,0 +1,26 @@
+namespace Closit.Models {
+    public class allEnums {
+        public enum Color
+        {
+            White, Yellow, Blue, Red, Green,
+            Black, Brown, Teal, Silver, Purple,
+            Gold, Gray, Orange, RoseGold, Pink
+        }
+        public enum Season
+        {
+            Spring, Summer, Autumn, Winter, All
+        }
+        public enum ClothingType
+        {
+            Shirt, Pant, Accessory, Hat
+        }
+        public enum ItemType
+        {
+            Clothing, Makeup, Shoe, Other
+        }
+        public enum Gender 
+        {
+            Male, Female, NonBinary, Boys, Girls
+        }
+    }
+}
