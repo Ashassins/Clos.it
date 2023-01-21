643e88e4cacaf0f1996a621765f3c8255977c29d
Ashassins <ashenassassinmc@gmail.com>
2023-01-21 05:29:59 -0500

    fix Data Struggles

---
+++ Closit/Controllers/ItemController.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.IO;
+// using System.Threading.Tasks;
+namespace Closit.Controllers {
+    // public class ItemController : Controller {
+    //     // [HttpPost, ActionName("Create")]
+    // }
+}
--- Closit/Models/Cloth.cs
+++ Closit/Models/Cloth.cs
@@ -4,14 +4,13 @@ namespace Closit.Models
 {
     public class Cloth {
         public String UUID;
-        public String listingName {get; set; }
-        public String givenName {get; set; }
-        public allEnums.Color[] colorList {get; set; }
-        public allEnums.Season season {get; set; }
-        public DateTime purchaseDate {get; set; }
-        public allEnums.Gender gender {get; set; }
-        public String brand {get; set; }
-        public String tags {get; set; }
+        public String listingName { get; set; }
+        public String givenName { get; set; }
+        public allEnums.Color[] colorList { get; set; }
+        public allEnums.Season season { get; set; }
+        public DateTime purchaseDate { get; set; }
+        public allEnums.Gender gender { get; set; }
+        public String brand { get; set; }
+        public String tags { get; set; }
     }
-
 }
